b8790a4a15ff236fb5bdec9106bbea8825aa9bae
redakpro <redakhettab.pro@gmail.com>
2022-02-21 11:57:20 +0100

    Rendu final

--- Conception_exam1.py
+++ Conception_exam1.py
@@ -1,24 +1,20 @@
 import datetime, time
+import operator
 
-tank = 0
 
 class Task:
 
-    def __init__(self, NAME, STATUS, PERIOD, EXECUTION_TIME, PRIORITY=0):
+    def __init__(self, NAME, STATE, PERIOD, EXECUTION_TIME, PRIORITY=0):
         self.NAME = NAME
-        self.STATUS = STATUS
+        self.STATE = STATE
         self.PERIOD = PERIOD
         self.EXECUTION_TIME = EXECUTION_TIME
         self.PRIORITY = PRIORITY  # 0 by default
 
         self.NEXT_DEADLINE = datetime.datetime.now()
         self.LAST_EXECUTED_TIME = datetime.datetime.now()
-        self.EXECUTION_DONE = 100
 
-        # if self.IS_INTERRUPTIBLE == True:
-        #     self.EXECUTION_DONE = 0
-        # else:
-        #     self.EXECUTION_DONE = 100  # Big number by default
+        self.EXECUTION_DONE = 100  # Big number by default
 
     def need_to_run(self):
 
@@ -36,57 +32,45 @@ class Task:
     def run(self):
 
         global timer
-        global tank
 
-        if self.STATUS == "waiting":
-            if self.PRIORITY == 1:
-                self.STATUS = "running"
-                print(str(timer) + "\t=> " + self.NAME + " completed without interruption.")
-                if self.NAME=="Pump 1":
-                    tank += 10
-                    print(tank)
-                time.sleep(self.EXECUTION_TIME)
-                self.NEXT_DEADLINE = self.NEXT_DEADLINE + datetime.timedelta(seconds=self.PERIOD)
-            else:
-                self.STATUS = "blocked"
+        print(str(timer) + "\t=> " + self.NAME + " " + str(self.EXECUTION_DONE / self.EXECUTION_TIME) + "%")
+        time.sleep(1)
+        self.EXECUTION_DONE += 1
 
-        if self.STATUS == "blocked":
-            self.STATUS = "waiting"
+        if self.EXECUTION_DONE == self.EXECUTION_TIME:
+            print(str(timer) + "\tTask " + self.NAME + " finished his job : 100%")
+            self.EXECUTION_DONE = 0
+            self.STATE = 'waiting'
+            self.NEXT_DEADLINE = self.NEXT_DEADLINE + datetime.timedelta(seconds=self.PERIOD)
+            return
 
-        if self.STATUS == "running":
-            print(str(timer) + "\t=> " + self.NAME + " " + "is running")
-        # else:
-        #     print(str(timer) + "\t=> " + self.NAME + " " + str(self.EXECUTION_DONE / self.EXECUTION_TIME) + "%")
-        #     time.sleep(1)
-        #     self.EXECUTION_DONE += 1
+        if self.EXECUTION_DONE < self.EXECUTION_TIME:
+            print(str(timer) + "\tTask " + self.NAME + " en cours")
 
-            if self.EXECUTION_DONE == self.EXECUTION_TIME:
-                print(str(timer) + "\tTask " + self.NAME + " finished his job : 100%")
-                self.EXECUTION_DONE = 0
-                self.STATUS = "waiting"
-                self.NEXT_DEADLINE = self.NEXT_DEADLINE + datetime.timedelta(seconds=self.PERIOD)
-                return
+        if self.EXECUTION_DONE > self.EXECUTION_TIME:
+            print(str(timer) + "\tTask " + self.NAME + " est bloqué")
 
-            if self.NEXT_DEADLINE > datetime.datetime.now():
-                print(str(timer) + "\tTask " + self.NAME + " finished his job : " + str(
-                    self.EXECUTION_DONE / self.EXECUTION_TIME) + "%")
-                self.EXECUTION_DONE = 0
-                self.NEXT_DEADLINE = self.NEXT_DEADLINE + datetime.timedelta(seconds=self.PERIOD)
-                return
+        if self.NEXT_DEADLINE > datetime.datetime.now():
+            print(str(timer) + "\tTask " + self.NAME + " finished his job : " + str(
+                self.EXECUTION_DONE / self.EXECUTION_TIME) + "%")
+            self.EXECUTION_DONE = 0
+            self.NEXT_DEADLINE = self.NEXT_DEADLINE + datetime.timedelta(seconds=self.PERIOD)
+            return
 
 
 if __name__ == "__main__":
 
     # Definition of all tasks and instanciation
     task_list = [
-        Task(NAME='Pump 1', PERIOD=5, EXECUTION_TIME=2, PRIORITY=1, STATUS="waiting"),
-        Task(NAME='Pump 2', PERIOD=15, EXECUTION_TIME=3, PRIORITY=1, STATUS="waiting"),
-        Task(NAME='Machine 1', PERIOD=5, EXECUTION_TIME=5, PRIORITY=2, STATUS="waiting"),
-        Task(NAME='Machine 2', PERIOD=5, EXECUTION_TIME=3, PRIORITY=2, STATUS="waiting")]
-
+        Task(NAME="Pump 1", STATE="waiting", PERIOD=5, EXECUTION_TIME=2, PRIORITY=1),
+        Task(NAME="Pump 2", STATE="waiting", PERIOD=15, EXECUTION_TIME=3, PRIORITY=1),
+        Task(NAME="Machine 1", STATE="waiting", PERIOD=5, EXECUTION_TIME=5, PRIORITY=1),
+        Task(NAME="Machine 2", STATE="waiting", PERIOD=5, EXECUTION_TIME=3, PRIORITY=1)]
 
     global timer
+    global tank
     timer = -1
+    tank = -1
 
     # Global scheduling loop
     while (True):
@@ -94,25 +78,21 @@ if __name__ == "__main__":
         task_to_run = None
         task_priority = 0
         timer += 1
+        tank += 1
+        oil = 0
 
         # Choose the task to be run
         for current_task in task_list:
 
-            current_task_need_to_run = current_task.need_to_run()
+                if current_task.STATE == 'waiting':
+                    if (task_list[1].NAME == 'Pump 2' and task_list[1].STATE == 'waiting') or (
+                            task_list[2].NAME == 'Machine 1' and task_list[2].STATE == 'waiting') or (
+                            task_list[3].NAME == 'Machine 2' and task_list[3].STATE == 'waiting'):
+                        task_priority = current_task.PRIORITY
+                        task_to_run = current_task
+                        task_to_run.run()
 
-            # Always priority to tasks that is not interruptible : Round Robin
-            if current_task_need_to_run == True and current_task.PRIORITY > task_priority:
-                task_to_run = current_task
-                task_priority = current_task.PRIORITY
-                break
-
-            # Else choose among tasks using priority
-            # if current_task.IS_INTERRUPTIBLE == True and current_task_need_to_run == True and current_task.PRIORITY > task_priority:
-            #     task_priority = current_task.PRIORITY
-            #     task_to_run = current_task
 
         if task_to_run == None:
             time.sleep(1)
             print(str(timer) + "\tIdle")
-        else:
-            task_to_run.run()
